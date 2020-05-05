82339798975519ccb2fb8ee65dc606954a3a4009
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-05-05 15:12:43 -0700

    End workout button now functional

--- models/workout.js
+++ models/workout.js
@@ -3,12 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for workout"
-  },
-  exercises: Array
+  exercises: { type: Array },
+  date_start: { type: Date, default: Date.now },
+  date_end: { type: Date, required: false}
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,6 +2,7 @@
 // const dayOfYear = moment().dayOfYear();
 // const momentTimer = moment("2020-05-04T16:47:11-07:00").fromNow(); // Store and get the time for each workout
 // $("#momentTimer").html('started '+momentTimer);
+var workoutId
 
 $('.ui.dropdown').dropdown();
 $('select.dropdown').dropdown();
@@ -9,13 +10,25 @@ $('select.dropdown').dropdown();
 $('#addWorkoutButton').click(function() {
   event.preventDefault();
   console.log( "addWorkoutButton called." );
+  // Ajax POST to save Workout
+  $.ajax("/api/workout/", {
+    type: "POST"
+  }).then(
+    function() {
+      console.log("Created New Exercise!");
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
 });
+
 $('#updateWorkoutButton').click(function() {
   event.preventDefault();
   console.log( "updateWorkoutButton called." );
   $('#updateWorkoutButton .icon').toggleClass('check');
   setTimeout(function(){$('#updateWorkoutButton .icon').toggleClass('check')}, 1200)
 });
+
 $('#addFieldButton').click(function() {
   event.preventDefault();
   var unitId = $(".newExerciseUnit").last().data();
@@ -83,7 +96,6 @@ $('#addExerciseButton').click(function() {
       location.reload();
     }
   );
-
 });
 
 $('.exerciseButton').click(function() {
@@ -93,9 +105,25 @@ $('.exerciseButton').click(function() {
 $( "#addRoutineSelect" ).change(function() {
     console.log($( "#addRoutineSelect option:selected" ).data())
 })
+
 $( "#addExerciseSelect" ).change(function() {
-  // Add an Exercise
+  // Add Exercise to Workout
   var exerciseId = $( "#addExerciseSelect option:selected" ).data().id;
+  const workoutData = {}
+  workoutData.measurements = []
+  workoutData.measurements.push(exerciseId);
+  // POST exercise to workout
+  $.ajax("/api/workout/"+workoutId, {
+    type: "POST",
+    data: workoutData
+  }).then(
+    function() {
+      console.log(`Added ${exerciseId} to ${workoutId} `);
+      // Reload the page
+      location.reload();
+    }
+  );
+  // GET exercise data
   $.ajax({
     url: "api/exercise/"+exerciseId,
     method: "GET"
@@ -152,13 +180,20 @@ $( "#addExerciseSelect" ).change(function() {
 })
 
 // Customize Routines
-// Dropdown change
 $( "#routineSelect" ).change(function() {
   console.log($( "#routineSelect option:selected" ).val())
 })
 
 // On page load
-$(document).ready(function() {
+$(document).ready(async function() {
+  // Get workout data
+  var workoutResponse = await $.ajax({
+    url: "api/workout/",
+    method: "GET"
+  }).then(function(response) {
+    return response;
+  })
+  //console.log(workoutResponse[0]._id)
 
   // Return routines
   $.ajax({
@@ -188,77 +223,24 @@ $(document).ready(function() {
     });
   });
 
-});
-
-
-
-// Create a new workout, or continue last workout.
-
-// Add exercises to a previous workout plan.
-
-/* 
-// Return current workout
-fetch("/api/workout")
-  .then(response => response.json())
-  .then(data => {
-    // save db data on global variable
-    workouts = data;
-    populateTable();
-    storeWorkout();
-  });
-*/
-
-function storeWorkout() {
-  const nameEl = document.querySelector("#t-name");
-  const amountEl = document.querySelector("#t-amount");
-  const errorEl = document.querySelector(".error");
-
-  // validate form
-  if (nameEl.value === "" || amountEl.value === "") {
-    errorEl.textContent = "Missing Information";
-    return;
-  } else {
-    errorEl.textContent = "";
+  if(!workoutResponse[0].date_end){
+    var endButton = `<button class="ui labeled icon red button right floated" id="endWorkoutButton" type="submit">
+        <i class="icon stopwatch"></i>
+        End Workout
+      </button>`;
+    $("#endWorkout").append(endButton);
   }
-
-  // create record
-  const workout = {
-    name: nameEl.value,
-    value: amountEl.value,
-    date: new Date().toISOString()
-  };
-
-  // add to beginning of current array of data
-  workouts.unshift(workout);
-
-  // re-run logic to populate ui with new record
-  populateTable();
-
-  // also send to server
-  fetch("/api/workout", {
-    method: "POST",
-    body: JSON.stringify(workout),
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (data.errors) {
-        errorEl.textContent = "Missing Information";
-      } else {
-        // clear form
-        nameEl.value = "";
-        amountEl.value = "";
+  $('#endWorkoutButton').click(function() {
+    event.preventDefault();
+    var endData = {_id: workoutResponse[0]._id, date_end: new Date(Date.now()) };
+    $.ajax("/api/workout/"+workoutResponse[0]._id, {
+      type: "PUT",
+      data: endData
+    }).then(
+      function() {
+        location.reload();
       }
-    })
-    .catch(err => {
-      // fetch failed, so save in indexed db
-      saveRecord(workout);
+    );
+  })
 
-      // clear form
-      nameEl.value = "";
-      amountEl.value = "";
-    });
-}
+});
